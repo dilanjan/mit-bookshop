77e2ff21a316ebb5c5fb8a8cc47874b4e5817820
indikaucsc <kamal.indika.n@gmail.com>
2024-10-24 23:49:49 +0800

    modifications

--- src/config.php
+++ src/config.php
@@ -1,9 +1,13 @@
 <?php
 
-define('BASE_URL', 'http://localhost/mit-bookshop/');
+define('BASE_URL', 'http://local.bookshop.com/');
+// define('BASE_URL', 'http://localhost/mit-bookshop/');
 
-
-$servername = 'localhost';
-$username = 'awo';
-$password = 'password';
-$dbname = 'bookshop';
+$servername = '192.168.70.4';
+$username = 'lara';
+$password = 'lara';
+$dbname = 'lara';
+// $servername = 'localhost';
+// $username = 'awo';
+// $password = 'password';
+// $dbname = 'bookshop';
